1e53a9a4d15d244f205c33a28f7d4e5e8d8188bf
HectorOC-e <65907557+HectorOC-e@users.noreply.github.com>
2020-12-08 15:01:51 -0600

    Eliminación Conexion Stock

--- Proyecto BDII PRO/Clase II Parcial/Proyecto/Stock.vb
+++ Proyecto BDII PRO/Clase II Parcial/Proyecto/Stock.vb
@@ -5,24 +5,6 @@ Public Class Stock
     Public dr As SqlDataReader
 
     Private Sub Stock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Try
-            abrirConexion()
-            conexion.Open()
-            comando = New SqlCommand("Select count(*) as paquete1 from Center.stock where puesto='Gerente'")
-
-
-
-            'Dim mostrar As String = "select st.IdCodigo, st.identidad , st.nombre, st.puesto, te.modelo,te.tipoPaquete,te.Descripcion, te.cantidad from Center.stock st inner join Center.telefonos te on st.idPuesto = te.id"
-
-            'If conexion.MostrarAsignacion(mostrar) = False Then
-            'MessageBox.Show("No se ha hecho ninguna asignacion", "Error de carga", MessageBoxButtons.OK, MessageBoxIcon.Error)
-
-            'End If
-            llenarDataGridStock(DGstock)
-            conexion.Close()
-        Catch ex As Exception
-            MessageBox.Show("no se lleno por: " + ex.ToString)
-        End Try
     End Sub
 
 End Class
